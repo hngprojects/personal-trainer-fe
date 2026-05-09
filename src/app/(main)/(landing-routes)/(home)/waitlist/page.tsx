3d0d37eee0546e2198080cac66ecf3cf9df36f56
Ezeike David Chukwunonso <114363324+Cr8tivedav@users.noreply.github.com>
2026-05-09 17:56:16 +0100

    feat: add marketing sections to Squeeze page
    Built out Fitness Lifestyle and Trainer Footer sections.

---
+++ src/app/(main)/(landing-routes)/(home)/waitlist/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import FitnessLifestyle from '~/components/squeeze/FitnessLifestyle'
+import SqueezeFooter from '~/components/squeeze/SqueezeFooter'
+
+const WaitList = () => {
+  return (
+    <div>
+      <FitnessLifestyle />
+      <SqueezeFooter />
+    </div>
+  )
+}
+
+export default WaitList
